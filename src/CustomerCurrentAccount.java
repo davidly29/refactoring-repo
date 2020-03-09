fdb13061988a68eae643fa8831083f89584e96f9
davidly29 <44612379+davidly29@users.noreply.github.com>
2020-03-09 20:16:52 +0000

    Commit 5.3 ---- Moved validation of current account to static method in the CustomerCurrentAccount class.

--- src/CustomerCurrentAccount.java
+++ src/CustomerCurrentAccount.java
@@ -1,4 +1,6 @@
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class CustomerCurrentAccount extends CustomerAccount 
 {
@@ -26,5 +28,59 @@ public void setAtm(ATMCard atm)
 {
 	this.atm = atm;
 }
+
+public static boolean validateAccount(CustomerAccount acc) {
+	int count = 3;
+	int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
+	boolean loop = true;
+	boolean on = true;
+
+	while(loop)
+	{
+		if(count == 0)
+		{
+			JOptionPane.showMessageDialog(null, "Pin entered incorrectly 3 times. ATM card locked."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);
+			((CustomerCurrentAccount) acc).getAtm().setValid(false);
+			// customer(loggedInCustomer);
+			return false;
+		}
+
+		String Pin = JOptionPane.showInputDialog(null, "Enter 4 digit PIN;");
+		int i = Integer.parseInt(Pin);
+
+		if(on)
+		{
+			if(checkPin == i)
+			{
+				loop = false;
+				JOptionPane.showMessageDialog(null, "Pin entry successful" ,  "Pin", JOptionPane.INFORMATION_MESSAGE);
+				return true;
+			}
+			else
+			{
+				count --;
+				JOptionPane.showMessageDialog(null, "Incorrect pin. " + count + " attempts remaining."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);
+			}
+
+		}
+
+	}
+	return true;
+	}
+
+	public static void makeDeposit(CustomerAccount acc, double balance) {
+		String euro = "\u20ac";
+		acc.setBalance(acc.getBalance() + balance);
+		Date date = new Date();
+		String date2 = date.toString();
+		String type = "Lodgement";
+		double amount = balance;
+
+		AccountTransaction transaction = new AccountTransaction(date2, type, amount);
+		acc.getTransactionList().add(transaction);
+
+		JOptionPane.showMessageDialog(null, balance + euro + " added do you account!" ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "New balance = " + acc.getBalance() + euro ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
+	}
 
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1310,78 +1310,27 @@ public class Menu extends JFrame{
 
 					lodgementButton.addActionListener(new ActionListener(  ) {
 						public void actionPerformed(ActionEvent ae) {
-							boolean loop = true;
-							boolean on = true;
+							boolean on = false;
 							double balance = 0;
 
 							if(acc instanceof CustomerCurrentAccount)
 							{
-								int count = 3;
-								int checkPin = ((CustomerCurrentAccount) acc).getAtm().getPin();
-								loop = true;
+                                on = CustomerCurrentAccount.validateAccount(acc);
+							}
 
-								while(loop)
-								{
-									if(count == 0)
-									{
-										JOptionPane.showMessageDialog(mainFrame, "Pin entered incorrectly 3 times. ATM card locked."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);
-										((CustomerCurrentAccount) acc).getAtm().setValid(false);
-										customer(loggedInCustomer);
-										loop = false;
-										on = false;
-									}
-
-									String Pin = JOptionPane.showInputDialog(mainFrame, "Enter 4 digit PIN;");
-									int i = Integer.parseInt(Pin);
-
-									if(on)
-									{
-										if(checkPin == i)
-										{
-											loop = false;
-											JOptionPane.showMessageDialog(mainFrame, "Pin entry successful" ,  "Pin", JOptionPane.INFORMATION_MESSAGE);
-
-										}
-										else
-										{
-											count --;
-											JOptionPane.showMessageDialog(mainFrame, "Incorrect pin. " + count + " attempts remaining."  ,"Pin",  JOptionPane.INFORMATION_MESSAGE);
-										}
-
-									}
-								}
-
-
-							}		if(on)
+							if(on)
 							{
-								String balanceTest = JOptionPane.showInputDialog(mainFrame, "Enter amount you wish to lodge:");//the isNumeric method tests to see if the string entered was numeric.
+								String balanceTest = JOptionPane.showInputDialog(mainFrame, "Enter amount you wish to lodge:");
 								if(isNumeric(balanceTest))
 								{
 									balance = Double.parseDouble(balanceTest);
-									loop = false;
 								}
 								else
 								{
 									JOptionPane.showMessageDialog(mainFrame, "You must enter a numerical value!" ,"Oops!",  JOptionPane.INFORMATION_MESSAGE);
 								}
 
-
-								String euro = "\u20ac";
-								acc.setBalance(acc.getBalance() + balance);
-								// String date = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
-								Date date = new Date();
-								String date2 = date.toString();
-								String type = "Lodgement";
-								double amount = balance;
-
-
-
-
-								AccountTransaction transaction = new AccountTransaction(date2, type, amount);
-								acc.getTransactionList().add(transaction);
-
-								JOptionPane.showMessageDialog(mainFrame, balance + euro + " added do you account!" ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
-								JOptionPane.showMessageDialog(mainFrame, "New balance = " + acc.getBalance() + euro ,"Lodgement",  JOptionPane.INFORMATION_MESSAGE);
+								CustomerCurrentAccount.makeDeposit(acc, balance);
 							}
 
 						}
